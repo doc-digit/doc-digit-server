d34d2ce691ab8b2ca1a065ae7bc8672771100da5
duszekxxxx <duszekxxxx>
2019-03-17 15:04:02 +0100

    Creating class to auto rotating given image

---
+++ rotate-microservice/src/main/java/com/springbootdev/springcloud/stream/examples/consumer/Funcionality/RotateClassInterface.java
@@ -0,0 +1,8 @@
+package com.springbootdev.springcloud.stream.examples.consumer.Funcionality;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public interface RotateClassInterface {
+    BufferedImage rotateImage(BufferedImage image);
+}
