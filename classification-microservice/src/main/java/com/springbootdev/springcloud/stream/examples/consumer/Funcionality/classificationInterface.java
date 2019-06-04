9082051f5bc9fce9a57c07663edc37e019cc5adc
duszekxxxx <duszekxxxx>
2019-06-04 23:10:13 +0200

    Add classificationInterface

---
+++ classification-microservice/src/main/java/com/springbootdev/springcloud/stream/examples/consumer/Funcionality/classificationInterface.java
@@ -0,0 +1,7 @@
+package com.springbootdev.springcloud.stream.examples.consumer.Funcionality;
+
+import com.lowagie.text.Document;
+
+public interface classificationInterface {
+    String classify(Document document);
+}
